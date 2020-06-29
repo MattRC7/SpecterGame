1f75bb38be9256f4494877b8834c869dead33b4b
Matthew Cardarelli <mattrc7@gmx.com>
2020-06-28 21:21:00 -0400

    Prettier health bars

--- LifeBar.gd
+++ LifeBar.gd
@@ -4,6 +4,7 @@ extends ColorRect
 const DEFAULT_MAX_LENGTH := 400.0
 
 onready var tween: Tween = get_node("Tween")
+onready var backbar: ColorRect = get_node("BackBar")
 
 var max_life := 1
 var max_length := 1.0
@@ -32,3 +33,4 @@ func reset_bar(life_force: LifeForce, scale_factor = 1.0) -> void:
 	self.max_length = DEFAULT_MAX_LENGTH * scale_factor
 	var target_length = _compute_target_length(life_force.current)
 	self.margin_right = self.margin_left + target_length as int
+	backbar.margin_right = backbar.margin_left + max_length
