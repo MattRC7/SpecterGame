adef035a5ba6557bac5dc44ea8ed9ac98b68f654
Matthew Cardarelli <mattrc7@gmx.com>
2020-08-03 22:03:39 -0400

    Transition from battle back to cutscene

--- PlayerAvatar.gd
+++ PlayerAvatar.gd
@@ -1,22 +1,37 @@
+class_name PlayerAvatar
 extends Sprite
 
+const speed = 4.0
+
 enum Direction {
-	IDLE = 0,
+	DOWN = 0,
 	UP = 1,
-	DOWN = 2,
-	RIGHT = 3
-	LEFT = 4
+	RIGHT = 2
+	LEFT = 3
 }
 
-var direction: int = Direction.IDLE
-var distance: int = 0
+onready var tween = get_node("Tween")
+
+var direction: int = Direction.UP
+var frozen := false
 
 func _unhandled_key_input(event):
-	if (distance == 0):
+	if (!frozen && !tween.is_active()):
 		if (event.is_action_pressed("ui_left", true)):
-			_set_direction(Direction.LEFT)
+			set_direction(Direction.LEFT)
+			tween.interpolate_property(self, "position", null, Vector2(self.position.x - 16, self.position.y), 1.0/speed, Tween.TRANS_LINEAR)
+		if (event.is_action_pressed("ui_right", true)):
+			set_direction(Direction.RIGHT)
+			tween.interpolate_property(self, "position", null, Vector2(self.position.x + 16, self.position.y), 1.0/speed, Tween.TRANS_LINEAR)
+		if (event.is_action_pressed("ui_up", true)):
+			set_direction(Direction.UP)
+			tween.interpolate_property(self, "position", null, Vector2(self.position.x, self.position.y - 16), 1.0/speed, Tween.TRANS_LINEAR)
+		if (event.is_action_pressed("ui_down", true)):
+			set_direction(Direction.DOWN)
+			tween.interpolate_property(self, "position", null, Vector2(self.position.x, self.position.y + 16), 1.0/speed, Tween.TRANS_LINEAR)
+		if (!tween.is_active()):
+			tween.start()
 
-func _set_direction(new_direction: int):
+func set_direction(new_direction: int):
 	direction = new_direction
 	self.frame = new_direction
-	distance = 16
