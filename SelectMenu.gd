be043fbe28dfd9280471d61b2f55cdbd2f0d9996
Matthew Cardarelli <mattrc7@gmx.com>
2020-08-09 23:02:37 -0400

    Refactored option descriptions

--- SelectMenu.gd
+++ SelectMenu.gd
@@ -2,30 +2,28 @@ class_name SelectMenu
 extends BoxContainer
 
 var options: ItemList
-var action_list: Array
 onready var description: Label = get_node("SelectMenuOptionText")
 
 func _ready():
 	options = get_node("SelectMenuOptions")
 
-func _display_actions() -> void:
+func _display_actions(actions: Array) -> void:
 	options.clear()
-	for action in action_list:
+	for action in actions:
 		options.add_item(action.text)
 
-func _display_action_description(index: int):
-	description.text = action_list[index].description
+func _display_action_description(index: int, actions: Array):
+	description.text = actions[index].description
 
 func get_player_choice(actions: Array) -> String:
-	action_list = actions
-	_display_actions()
+	_display_actions(actions)
 	options.visible = true
 	description.visible = true
-	options.connect("item_selected", self, "_display_action_description")
+	options.connect("item_selected", self, "_display_action_description", [actions])
 	options.select(0)
-	_display_action_description(0)
+	_display_action_description(0, actions)
 	var selection_index = yield(options, "item_activated")
 	options.disconnect("item_selected", self, "_display_action_description")
 	options.visible = false
 	description.visible = false
-	return action_list[selection_index].key
+	return actions[selection_index].key
