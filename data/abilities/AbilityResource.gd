49058f4434eeca4a07c2217459d666599885f72e
Matthew Cardarelli <mattrc7@gmx.com>
2020-10-25 18:39:28 -0400

    Added flavor text

--- data/abilities/AbilityResource.gd
+++ data/abilities/AbilityResource.gd
@@ -2,8 +2,9 @@ class_name AbilityResource
 extends Resource
 
 export var key: String
-export var name: String
-export var description: String
+export var name: String = 'Ability'
+export var description: String = 'An ability'
+export var action_text: String = 'uses an ability!'
 export var type: String
 export var inflict_damage := false
 export var damage: int
