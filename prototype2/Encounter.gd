2b223299da000fb9346efef748d4d3be3e14ab21
Matthew Cardarelli <mattrc7@gmx.com>
2020-12-13 09:55:45 -0500

    Added dying to plants

--- prototype2/Encounter.gd
+++ prototype2/Encounter.gd
@@ -5,5 +5,13 @@ func _ready():
 	get_tree().create_timer(5.0).connect("timeout", self, "attack");
 
 func attack():
-	get_tree().call_group("plant", "take_damage", 5)
+	get_tree().call_group("plant", "take_damage", 3)
 	get_tree().create_timer(5.0).connect("timeout", self, "attack");
+
+func _unhandled_input(event):
+	if event.is_action_pressed("game_sow"):
+		if event is InputEventMouse:
+			var plant_res: PackedScene = load('res://Plant.tscn');
+			var new_plant: Plant = plant_res.instance()
+			new_plant.position = event.position
+			self.add_child(new_plant)
--- prototype2/Plant.gd
+++ prototype2/Plant.gd
@@ -5,7 +5,13 @@ onready var plant_sprite: Sprite = get_node("PlantSprite");
 onready var seed_sprite: Sprite = get_node("SeedSprite");
 onready var hp_label: Label = get_node("HPLabel");
 
+enum {
+	SEED
+	PLANT
+}
+
 var life = 1;
+var state = SEED;
 
 func _ready():
 	get_tree().create_timer(3.0).connect("timeout", self, "sprout")
@@ -17,8 +23,11 @@ func sprout():
 	seed_sprite.visible = false;
 	plant_sprite.visible = true;
 	life = 10;
+	state = PLANT;
 
 func take_damage(damage: int):
+	if state == SEED:
+		return;
 	life = max(0, life - damage);
 	if (life == 0):
 		self.queue_free()
