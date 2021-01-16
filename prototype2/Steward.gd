a47b543f39dd9f620f450787a3e294d0b54ac6cc
Matthew Cardarelli <mattrc7@gmx.com>
2021-01-15 19:29:08 -0500

    Give doggo a cute animation

--- prototype2/Steward.gd
+++ prototype2/Steward.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 const SPEED := 600.0;
 
+onready var animator := get_node("StewardAnimator");
+
 func _ready():
 	position = get_viewport().get_mouse_position();
+	animator.play("float")
 
 func _process(delta):
 	position = position.move_toward(get_viewport().get_mouse_position(), delta*SPEED) 
